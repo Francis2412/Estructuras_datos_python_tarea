d2eabac2600ce2d8a010abbebabe2e8f6c8bfa36
Francis2412 <francisalejandrasm@gmail.com>
2026-09-24 21:08:48 -0600

    Primer commit

---
+++ conjunto.py
@@ -0,0 +1,4 @@
+# un conjunto es una colección de elementos no ordenados
+# Son  herterogéneos per solo con elementos inmutables
+# Mutables
+#Sin repeteción
---
+++ tuplas.py
@@ -0,0 +1,14 @@
+#Las tuplas son estructuras de datos en Python que permiten almacenar múltiples elementos en 
+#un solo objeto. A diferencia de las listas, las tuplas son inmutables, lo que significa
+#que no pueden modificarse después de ser creadas. Esto las hace ideales para representar
+#datos constantes o que no deben cambiar durante la ejecución del programa.
+  
+#Otro dato es que en las listas se usan [] y en las tuplas ()
+
+nombre = input("Ingrese su 1er nombre: ")
+apellido = input("Ingrese su 1er apellido: ")
+nombre_completo=(nombre, apellido )
+
+print(f"Nombre del usuario: {nombre_completo[0]}")
+print(f"Apellido del usuario: {{nombre_completo[1]}}")
+
